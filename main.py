3aa6ddeb8b36bfb8a265e1ef91981d343143ce6f
prasad1231 <69751158+prasad1231@users.noreply.github.com>
2022-09-02 19:25:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 
 def count_batteries_by_usage(cycles):
-  return {
-    "lowCount": 0,
-    "mediumCount": 0,
-    "highCount": 0
-  }
+  #Initializing the count of all batteries to zero in a dictionary.
+  dict_battery={
+        "lowCount":0,
+        "mediumCount":0,
+        "highCount":0
+    }
+  #Parsing through cycles[list] to check how many times the battery has been charged.
+  for charged in cycles:
+    if charged >= 0 and charged < 400:      
+      dict_battery["lowCount"] += 1   
+    elif charged >= 400 and charged < 920:               #920 is not included in this elif
+      dict_battery["mediumCount"] += 1
+    else:
+      dict_battery["highCount"] += 1                     #920 and more times
+         
+  return dict_battery
+   
+    
+
 
 
 def test_bucketing_by_number_of_cycles():
